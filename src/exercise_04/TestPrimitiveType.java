42483f47b24a29209295b908fb02233e929c3eb1
SungHoon Moon <moon9342@gmail.com>
2018-03-02 15:06:20 +0900

    commit

---
+++ src/exercise_04/TestPrimitiveType.java
@@ -0,0 +1,31 @@
+package exercise_04;
+
+public class TestPrimitiveType {
+
+	public static void main(String[] args){
+		// boolean type
+		boolean flag = true;
+		System.out.println( "value of flag = " + flag );
+		
+		// char type
+		char ch = 'A';
+		char ch2 = '\uAC00';
+		System.out.println( "value of flag = " + ch );
+		System.out.println( "value of flag = " + ch2 );
+		
+		// int type
+		int index = 3;
+		System.out.println( "value of index = " + index );
+		int i = 30;
+		long l = 30L;
+		System.out.println( "value of i is : " + i );
+		System.out.println( "value of l is : " + l );
+		
+		// double type
+		double d = 3.4;
+		System.out.println( "value of d = " + d );
+		float f = 3.14f;
+		System.out.println( "value of f is : " + f );
+		
+	}
+}
